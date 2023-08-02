a14f7ab618dcd47d8e3669313804cacc94f88fc4
Terry Cai <gidcai@gmail.com>
2023-08-02 18:47:46 +0800

    feat(web): web testing

--- mor.config.js
+++ mor.config.js
@@ -45,4 +45,31 @@ export default defineConfig([
   //   },
   //   plugins: [new MorJSPluginImageReplace()]
   // }
+  /**
+   * 支付宝小程序转 Web 编译配置
+   */
+  // {
+  //   name: 'web',
+  //   sourceType: 'alipay',
+  //   target: 'web',
+  //   globalObject: 'customMy',
+  //   web: {
+  //     showBack: true,
+  //     responsiveRootFontSize: 16,
+  //     // devServer: {
+  //     //   host: 'local-ip',
+  //     //   port: 8888
+  //     // },
+  //     appConfig: {
+  //       apiNoConflict: false,
+  //       components: {
+  //         map: {
+  //           sdk: '', // 地图 sdk 地址
+  //           version: '', // 地图 sdk 版本
+  //           key: '' // 个人地图 key
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
 ])
