e5bb10ba879d492d38858aa6333d26240fafb039
Terry Cai <gidcai@gmail.com>
2023-08-04 23:32:03 +0800

    feat(utils): add dnaDir func

--- scripts/utils.js
+++ scripts/utils.js
@@ -27,3 +27,17 @@ export const getDate = () => {
     .replace(/[-:T.]/g, '')
     .slice(0, 14)
 }
+
+/**
+ * 生成目录 dna 路径
+ * @param {*} dir
+ * @returns
+ * @example
+ */
+export const dnaDir = (dir, ver, split = '') => {
+  const hash = crypto.createHash('md5')
+  hash.update(dir)
+  hash.update(ver)
+  const dna = hash.digest('base64').replace(/=/g, '').slice(0, 6)
+  return split ? `${dir}${split}${dna}` : `${dir}${dna}`
+}
